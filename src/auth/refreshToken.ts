10aed06063a413925437fb7c0ae64f06343bd32f
GameFuzzy <awsmajed7@gmail.com>
2021-05-10 14:59:24 +0200

    Add expires property to cookie so that it's not a session cookie

--- src/auth/refreshToken.ts
+++ src/auth/refreshToken.ts
@@ -16,6 +16,10 @@ export const sendRefreshToken = (res: Response, token: string) => {
   res.cookie('jid', token, {
     httpOnly: true,
     sameSite: 'lax',
-    path: '/refresh_token'
+    path: '/refresh_token',
+    expires: new Date(
+      Date.now() +
+        1000 * 60 * 60 * 24 * 7 /* Seven days, same as the JWT token */
+    )
   })
 }
